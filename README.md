7e7b28a7c9ee138b43fc3c5768019365d7a4936b
Hiroshi Omata <op.homata@gmail.com>
2024-01-14 22:03:24 +0900

    Add poetry run

--- README.md
+++ README.md
@@ -6,16 +6,38 @@ website-scraping-tools
 * [Python, scrapy でお祭りスクレイピング](https://qiita.com/takecore@github/items/35905779504016085801)
     * [takecore/festival-crawler](https://github.com/takecore/festival-crawler)
 
+### docker
+Docker build
 ```
 $ docker-compose up -d --build
 $ docker-compose exec scrapy bash
 ```
 
+scrapy project
 ```
 /usr/src/app# scrapy startproject scraping .
-/usr/src/app# scrapy genspider getHtml zenn.dev
+/usr/src/app# scrapy genspider getHtml ishikawa.lg.jp
 ```
 
+scrapy execute
 ```
 $ scrapy crawl getHtml
 ```
+
+### poetry
+
+```
+$ poetry install
+```
+
+scrapy project
+```
+$ poetry run scrapy startproject scraping .
+$ poetry run scrapy genspider getHtml ishikawa.lg.jp
+```
+
+scrapy execute
+```
+$ poetry run scrapy crawl getHtml
+```
+
--- scraping/spiders/getHtml.py
+++ scraping/spiders/getHtml.py
@@ -1,14 +1,24 @@
 import scrapy
 import os
+import datetime
+
 
 
 class GethtmlSpider(scrapy.Spider):
     name = "getHtml"
-    allowed_domains = ["zenn.dev"]
-    start_urls = ["https://zenn.dev/elletech/articles/nextjs_microcms"]
+    allowed_domains = ["ishikawa.lg.jp"]
+    start_urls = ["https://www.pref.ishikawa.lg.jp/"]
 
     def parse(self, response):
-        output_path = './downloads/'
-        os.makedirs(os.path.dirname(output_path), exist_ok=True)
-        with open(output_path + 'index.html', 'w', encoding='utf-8') as f:
+        t_delta = datetime.timedelta(hours=9)
+        JST = datetime.timezone(t_delta, 'JST')
+        dt_now = datetime.datetime.now(JST)
+        # dt_path = dt_now.strftime('%Y%m%d_%H%M%S%z')
+        dt_path = dt_now.strftime('%Y%m%d')
+
+        output_path = os.path.join('./downloads/', dt_path)
+        os.makedirs(output_path, exist_ok=True)
+
+        full_path = os.path.join(output_path, 'index.html')
+        with open(full_path, 'w', encoding='utf-8') as f:
             f.write(response.text)
